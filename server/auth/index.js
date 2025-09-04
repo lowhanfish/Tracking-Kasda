a1253f4bef0052bbcb187a5b8aeb3dbf2fcfd92f
lowhanfish <lowhanfish@gmail.com>
2025-09-05 00:04:03 +0800

    check user at database at login route

--- server/auth/index.js
+++ server/auth/index.js
@@ -29,9 +29,11 @@ const schema = Joi.object({
 
 
 const respondError422 = (res, next, text)=>{
-res.status(422);
+    res.status(422);
     const error = new Error(text);
-    next(error);
+    console.log("woooooooow : ",error)
+    // next(error);
+    next(text);
 }
 
 
@@ -49,79 +51,82 @@ router.post('/login', (req, res, next)=>{
 
     const result = schema.validate({ username: req.body.username, password: req.body.password });
     console.log(result);
-    res.json(result.error)
+    // res.json(result.error)
     
     
-    // if (result.error == null || result.error == undefined) {
-    //     let query = ` SELECT 
-    //         egov.users.*, 
-    //         simpeg.unit_kerja.unit_kerja as unit_kerja_nama,
-    //         simpeg.unit_kerja.alamat as unit_kerja_alamat,
+    if (result.error == null || result.error == undefined) {
+        let query = ` SELECT 
+            egov.users.*, 
+            simpeg.unit_kerja.unit_kerja as unit_kerja_nama,
+            simpeg.unit_kerja.alamat as unit_kerja_alamat,
 
-    //         simpeg.biodata.id as bio_id,
-    //         simpeg.biodata.nama as bio_nama,
-    //         simpeg.biodata.gelar_depan as bio_gelar_depan,
-    //         simpeg.biodata.gelar_belakang as bio_gelar_belakang,
-    //         simpeg.biodata.tempat_lahir  as bio_tempat_lahir ,
-    //         simpeg.biodata.ttl as bio_ttl,
-    //         simpeg.biodata.gol as bio_gol,
-    //         simpeg.biodata.jabatan as bio_jabatan ,
-    //         simpeg.biodata.alamat as bio_alamat,
-    //         simpeg.instansi.id as instansi_id,
-    //         simpeg.instansi.instansi as instansi_nama,
-
-
-    //         monev_pembangunan.menu_klp.akses_unit as akses_unit
+            simpeg.biodata.id as bio_id,
+            simpeg.biodata.nama as bio_nama,
+            simpeg.biodata.gelar_depan as bio_gelar_depan,
+            simpeg.biodata.gelar_belakang as bio_gelar_belakang,
+            simpeg.biodata.tempat_lahir  as bio_tempat_lahir ,
+            simpeg.biodata.ttl as bio_ttl,
+            simpeg.biodata.gol as bio_gol,
+            simpeg.biodata.jabatan as bio_jabatan ,
+            simpeg.biodata.alamat as bio_alamat,
+            simpeg.instansi.id as instansi_id,
+            simpeg.instansi.instansi as instansi_nama,
 
 
-          
-
-    //     FROM egov.users 
-
-    //     JOIN simpeg.unit_kerja 
-    //         ON egov.users.unit_kerja = simpeg.unit_kerja.id
-
-    //     JOIN simpeg.instansi 
-    //         ON simpeg.instansi.id = simpeg.unit_kerja.instansi
-
-    //     JOIN simpeg.biodata 
-    //         ON egov.users.nama_nip = simpeg.biodata.nip
-
-    //     LEFT JOIN monev_pembangunan.menu_klp 
-    //         ON monev_pembangunan.menu_klp.id = egov.users.monev_pembangunan
-
-    //     WHERE users.username = '`+req.body.username+`';`
-
-    //     db.query(query, (err, row)=>{
-    //         if (row.length <= 0) {
-    //             console.log("akun tidak di temukan");
-    //             respondError422(res, next, 'Akun tidak ditemukan')
-    //         } else {
-    //             var user = {};
-    //             row.forEach(user => {
-    //                 user = user;
-    //             });
-
-    //         }
-    //         const payload =  {
-    //                 _id: user.id,
-    //                 username : user.username,
-    //                 profile : {
-    //                     username : user.username,
-    //                     nama : user.nama,
-    //                     hp : user.hp,
-    //                     email : user.email,
-    //                     id_pengguna : user.id_pengguna,
-    //                 }
-    //             };
+            monev_pembangunan.menu_klp.akses_unit as akses_unit
 
 
-    //             // prepare bcrypt compare
+        FROM egov.users 
 
-    //     })
-    // } else {
-        
-    // }
+        JOIN simpeg.unit_kerja 
+            ON egov.users.unit_kerja = simpeg.unit_kerja.id
+
+        JOIN simpeg.instansi 
+            ON simpeg.instansi.id = simpeg.unit_kerja.instansi
+
+        JOIN simpeg.biodata 
+            ON egov.users.nama_nip = simpeg.biodata.nip
+
+        LEFT JOIN monev_pembangunan.menu_klp 
+            ON monev_pembangunan.menu_klp.id = egov.users.monev_pembangunan
+
+        WHERE users.username = '`+req.body.username+`';`
+
+        db.query(query, (err, row)=>{
+            // console.log(row)
+            if (row.length <= 0) {
+                console.log("akun tidak di temukan");
+                respondError422(res, next, 'Akun tidak ditemukan')
+                // res.json("Akun tidak ditemukan")
+            } else {
+                var user = {};
+                row.forEach(user => {
+                    user = user;
+                });
+
+                const payload =  {
+                        _id: user.id,
+                        username : user.username,
+                        profile : {
+                            username : user.username,
+                            nama : user.nama,
+                            hp : user.hp,
+                            email : user.email,
+                            id_pengguna : user.id_pengguna,
+                        }
+                    };
+            }
+
+
+                // prepare bcrypt compare
+
+        })
+    } else {
+        res.json(result.error.details[0].message)
+    }
+
+
+    // Berikutnya commit tentang check user at database, soalnya masih jam 23:32
 
 
 })
