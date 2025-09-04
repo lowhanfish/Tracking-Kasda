7dbcd96f0b9f6ff79975d5b16d79e27ef9d71448
lowhanfish <lowhanfish@gmail.com>
2025-09-04 22:15:28 +0800

    added router checkAuth

---
+++ server/api/checkAuth.js
@@ -0,0 +1,11 @@
+// const express = require('express');
+// const router = express.Router();
+
+
+import express from "express";
+const router = express.Router();
+
+
+router.post((req, res)=>{
+    res.json("OK")
+})
