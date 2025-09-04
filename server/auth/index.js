e5dcefdb632d9d51e117c0cd6a0555907eb8f1d8
lowhanfish <lowhanfish@gmail.com>
2025-09-04 22:59:51 +0800

    add db query for searcing user in login route

--- server/auth/index.js
+++ server/auth/index.js
@@ -49,7 +49,45 @@ router.post('/login', (req, res, next)=>{
     res.send(result)
 
     if (result.error == null || result.error == undefined) {
-        let query = ``
+        let query = ` SELECT 
+            egov.users.*, 
+            simpeg.unit_kerja.unit_kerja as unit_kerja_nama,
+            simpeg.unit_kerja.alamat as unit_kerja_alamat,
+
+            simpeg.biodata.id as bio_id,
+            simpeg.biodata.nama as bio_nama,
+            simpeg.biodata.gelar_depan as bio_gelar_depan,
+            simpeg.biodata.gelar_belakang as bio_gelar_belakang,
+            simpeg.biodata.tempat_lahir  as bio_tempat_lahir ,
+            simpeg.biodata.ttl as bio_ttl,
+            simpeg.biodata.gol as bio_gol,
+            simpeg.biodata.jabatan as bio_jabatan ,
+            simpeg.biodata.alamat as bio_alamat,
+            simpeg.instansi.id as instansi_id,
+            simpeg.instansi.instansi as instansi_nama,
+
+
+            monev_pembangunan.menu_klp.akses_unit as akses_unit
+
+
+          
+
+        FROM egov.users 
+
+        JOIN simpeg.unit_kerja 
+            ON egov.users.unit_kerja = simpeg.unit_kerja.id
+
+        JOIN simpeg.instansi 
+            ON simpeg.instansi.id = simpeg.unit_kerja.instansi
+
+        JOIN simpeg.biodata 
+            ON egov.users.nama_nip = simpeg.biodata.nip
+
+        LEFT JOIN monev_pembangunan.menu_klp 
+            ON monev_pembangunan.menu_klp.id = egov.users.monev_pembangunan
+
+        WHERE users.username = '`+req.body.username+`';`
+        
         db.query(query, (err, row)=>{
             if (row.length <= 0) {
                 console.log("akun tidak di temukan");
