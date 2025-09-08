0b47e4b97c4ccc80fe3d8b7209c10e38198dd5ae
lowhanfish <lowhanfish@gmail.com>
2025-09-08 19:16:57 +0800

    add helper rekursif to checkbox

--- admin/src/pages/master/userGroup/components/add.jsx
+++ admin/src/pages/master/userGroup/components/add.jsx
@@ -43,17 +43,19 @@ function AccessSettingAdd({ handleCloseModalAdd, typeEvent, formx, getData }) {
 
 
     const getHandle = () => {
-        axios.post(url.URL_GROUP + pathx, JSON.stringify(form), {
-            headers: {
-                'Content-Type': 'application/json',
-                'Authorization': `kikensbatara ${token}`
-            }
-        }).then((response) => {
-            console.log(response);
-        }).catch((error) => {
-            alert(JSON.stringify(error.response.data))
-            console.log(error)
-        })
+
+        console.log(listMenu)
+        // axios.post(url.URL_GROUP + pathx, JSON.stringify(form), {
+        //     headers: {
+        //         'Content-Type': 'application/json',
+        //         'Authorization': `kikensbatara ${token}`
+        //     }
+        // }).then((response) => {
+        //     console.log(response);
+        // }).catch((error) => {
+        //     alert(JSON.stringify(error.response.data))
+        //     console.log(error)
+        // })
     }
 
     const [listMenu, setListMenu] = useState([]);
@@ -72,6 +74,27 @@ function AccessSettingAdd({ handleCloseModalAdd, typeEvent, formx, getData }) {
             console.log(error)
         })
     }
+
+
+
+
+    // ========================== CHECKBOX ==========================
+
+    // 🔧 helper rekursif untuk update node berdasarkan id
+    const updateNode = (nodes, id, field, value) =>
+        nodes.map((node) =>
+            node.id === id
+                ? { ...node, [field]: value }
+                : { ...node, children: updateNode(node.children || [], id, field, value) }
+        );
+
+    // mirip v-model
+    const bindCheckbox = (id, field, setListMenu) => (e) => {
+        setListMenu((prev) => updateNode(prev, id, field, e.target.checked));
+    };
+
+
+    // ========================== CHECKBOX ==========================
 
 
 
@@ -152,10 +175,10 @@ function AccessSettingAdd({ handleCloseModalAdd, typeEvent, formx, getData }) {
                                             <tr className='tablex1'>
                                                 <td className=''>{index + 1}</td>
                                                 <td>{data.title}</td>
-                                                <td className='center'><input type="checkbox" value="Bike"></input></td>
-                                                <td className='center'><input type="checkbox" value="Bike"></input></td>
-                                                <td className='center'><input type="checkbox" value="Bike"></input></td>
-                                                <td className='center'><input type="checkbox" value="Bike"></input></td>
+                                                <td className='center'><input type="checkbox" checked={data.view} onChange={bindCheckbox(data.id, "view", setListMenu)}></input></td>
+                                                <td className='center'><input type="checkbox" checked={data.add} onChange={bindCheckbox(data.id, "add", setListMenu)}></input></td>
+                                                <td className='center'><input type="checkbox" checked={data.update} onChange={bindCheckbox(data.id, "update", setListMenu)}></input></td>
+                                                <td className='center'><input type="checkbox" checked={data.remove} onChange={bindCheckbox(data.id, "remove", setListMenu)}></input></td>
                                             </tr>
 
                                             {
@@ -165,20 +188,20 @@ function AccessSettingAdd({ handleCloseModalAdd, typeEvent, formx, getData }) {
                                                         <tr className='tablex2'>
                                                             <td className=''>{index + 1}.{index + 1}</td>
                                                             <td>{data1.title}</td>
-                                                            <td className='center'><input type="checkbox" value="Bike"></input></td>
-                                                            <td className='center'><input type="checkbox" value="Bike"></input></td>
-                                                            <td className='center'><input type="checkbox" value="Bike"></input></td>
-                                                            <td className='center'><input type="checkbox" value="Bike"></input></td>
+                                                            <td className='center'><input checked={data1.view} onChange={bindCheckbox(data1.id, "view", setListMenu)} type="checkbox"></input></td>
+                                                            <td className='center'><input checked={data1.add} onChange={bindCheckbox(data1.id, "add", setListMenu)} type="checkbox"></input></td>
+                                                            <td className='center'><input checked={data1.update} onChange={bindCheckbox(data1.id, "update", setListMenu)} type="checkbox"></input></td>
+                                                            <td className='center'><input checked={data1.remove} onChange={bindCheckbox(data1.id, "remove", setListMenu)} type="checkbox"></input></td>
                                                         </tr>
                                                         {
                                                             data1.children.map((data2, index2) => (
                                                                 <tr className='tablex3'>
                                                                     <td className=''>{index + 1}.{index + 1}.{index + 1}</td>
                                                                     <td>- {data2.title}</td>
-                                                                    <td className='center'><input type="checkbox" value="Bike"></input></td>
-                                                                    <td className='center'><input type="checkbox" value="Bike"></input></td>
-                                                                    <td className='center'><input type="checkbox" value="Bike"></input></td>
-                                                                    <td className='center'><input type="checkbox" value="Bike"></input></td>
+                                                                    <td className='center'><input type="checkbox" checked={data2.view} onChange={bindCheckbox(data2.id, "view", setListMenu)}></input></td>
+                                                                    <td className='center'><input type="checkbox" checked={data2.add} onChange={bindCheckbox(data2.id, "add", setListMenu)}></input></td>
+                                                                    <td className='center'><input type="checkbox" checked={data2.update} onChange={bindCheckbox(data2.id, "update", setListMenu)}></input></td>
+                                                                    <td className='center'><input type="checkbox" checked={data2.remove} onChange={bindCheckbox(data2.id, "remove", setListMenu)}></input></td>
                                                                 </tr>
 
                                                             ))
