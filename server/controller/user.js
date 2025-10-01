01b1adf606eaaa661fee330982bebf2de25340ea
lowhanfish <lowhanfish@gmail.com>
2025-10-01 23:53:09 +0800

    added getDataUser function

--- server/controller/user.js
+++ server/controller/user.js
@@ -1,10 +1,24 @@
 import db from "../db/mysql/index.js";
+import dbx from "../db/mysql/egov.js";
 import dbCondition from "../lib/dbCondition.js";
 import dbResolveCondition from "../lib/dbResolveCondition.js";
 
 
 export const getDataUser = (req, res) =>{
-    
+    const query = `
+    SELECT
+    users.username,
+    users.name,
+    users.email,
+    users.address,
+    users.phone,
+    users.createdAt,
+    users.createdBy
+    FROM users
+    `
+    dbx.query(query, (err, rows)=>{
+        dbCondition(res, err, rows)
+    })
 }
 
 
