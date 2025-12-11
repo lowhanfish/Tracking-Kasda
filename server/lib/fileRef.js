9dbfebba78e32f2d0ae946f8344bd7991b670f60
lowhanfish <lowhanfish@gmail.com>
2025-12-11 14:24:24 +0800

    Added delete file function

--- server/lib/fileRef.js
+++ server/lib/fileRef.js
@@ -3,14 +3,12 @@ import db from "../db/mysql/index.js";
 
 
 export const insertFile = (filetitle, type, db_name, ref_id ) => {
-
     return new Promise((resolve, reject) => {
         const query = `
             INSERT INTO ref_id (title, type, db_name, ref_id)
             VALUES (?, ?, ?, ?)
         `
         const values = [filetitle, type, db_name, ref_id];
-
         db.query(query, values, (err, rows)=> {
             if (err) {
                 reject({
@@ -25,9 +23,28 @@ export const insertFile = (filetitle, type, db_name, ref_id ) => {
             }
         })
     })
-
-
 }
-export const deleteFile = (req, res) => {
 
+
+export const deleteFile = (db_name, ref_id) => {
+    return new Promise((resolve, reject) => {
+        const query = `
+            DELETE FROM ref_file
+            WHERE db_name = ? AND ref_id = ?
+        `
+        const values = [db_name, ref_id]
+        db.query(query, values, (err, rows) => {
+            if (err) {
+                reject({
+                    status : 500,
+                    message : err
+                })
+            } else {
+                resolve({
+                    status : 500,
+                    message : rows
+                })
+            }
+        })
+    })
 }
