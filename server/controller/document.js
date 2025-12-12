be9aba744f86a3e24d2778fc91e912a1e16841f2
lowhanfish <lowhanfish@gmail.com>
2025-12-12 23:51:21 +0800

    Added add document function

--- server/controller/document.js
+++ server/controller/document.js
@@ -22,3 +22,24 @@ export const view = (req, res) => {
 
 
 }
+
+
+export const add = (req, res) => {
+    const query = `
+        SELECT
+        document.*
+        FROM document
+    
+    `
+
+    db.query((err, rows)=>{
+        if (err) {
+            console.log(err);
+            res.status(500).send(err)
+        } else {
+            res.status(200).send(rows)
+        }
+    })
+
+
+}
