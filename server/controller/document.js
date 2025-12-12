ce6cd635805585beb3612c877f1bed72d087b021
lowhanfish <lowhanfish@gmail.com>
2025-12-13 00:31:25 +0800

    modify  view document function

--- server/controller/document.js
+++ server/controller/document.js
@@ -1,5 +1,6 @@
 import db from "../db/mysql/index.js";
-
+import { view as view_pph, deletex as delete_pph } from "../controller/pph.js";
+import { view as view_ppn, deletex as delete_ppn } from "../controller/ppn.js";
 
 
 
@@ -8,14 +9,19 @@ export const view = (req, res) => {
         SELECT
         document.*
         FROM document
-    
     `
 
-    db.query((err, rows)=>{
+    db.query(async (err, rows)=>{
         if (err) {
             console.log(err);
             res.status(500).send(err)
         } else {
+            
+            for (let i = 0; i < rows.length; i++) {
+                rows[i].ppn = await view_ppn(document.id);
+                rows[i].pph = await view_pph(document.id);
+            }
+            
             res.status(200).send(rows)
         }
     })
