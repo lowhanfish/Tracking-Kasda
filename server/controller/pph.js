a47c5c85e6046b19c0289430916e01c099a891bb
lowhanfish <lowhanfish@gmail.com>
2025-12-13 00:26:41 +0800

    Added add pph function

--- server/controller/pph.js
+++ server/controller/pph.js
@@ -9,10 +9,36 @@ export const view = (documents_id) => {
         const query = `
             SELECT
             master_pph.*
-            FROM ppn
+            FROM pph
             LEFT JOIN master_pph
-            ON master_pph.id = ppn.master_pph_id
-            WHERE ppn.documents_id = ?
+            ON master_pph.id = pph.master_pph_id
+            WHERE pph.documents_id = ?
+        `
+        const values = [documents_id];
+    
+        db.query(query, values, (err, rows)=>{
+            if (err) {
+                console.log(err);
+                reject({
+                    status : 500,
+                    message : err
+                })
+            } else {
+                resolve({
+                    status : 200,
+                    message : rows
+                })
+            }
+        })
+    })
+}
+
+
+export const deletex = (documents_id) => {
+    return new Promise((resolve, reject) => {
+        const query = `
+            DELETE pph
+            WHERE pph.documents_id = ?
         `
         const values = [documents_id];
     
