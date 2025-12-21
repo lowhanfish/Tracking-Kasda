af0a173983b9f7fad2fc96517bebed1042ded6cf
lowhanfish <lowhanfish@gmail.com>
2025-12-21 13:07:52 +0800

    added view router verification

--- server/controller/verification.js
+++ server/controller/verification.js
@@ -1,9 +1,38 @@
 import db from "../db/mysql/index.js";
+import db_egov from "../db/mysql/egov.js";
+import db_simpeg from "../db/mysql/simpeg.js";
 
 
 
 export const view = (req, res)=>{
-    res.status(200).send("OK");
+    const query = `
+        SELECT 
+        documents.* 
+
+        FROM ${db}.documents documents
+
+        JOIN ${db}.master_jns_pencairan master_jns_pencairan
+        ON documents.master_jns_pencairan_id = master_jns_pencairan.id
+
+        JOIN ${db}.master_jns_pencairan_list master_jns_pencairan_list
+        ON master_jns_pencairan_list.master_jns_pencairan_id = master_jns_pencairan.id
+
+    `
+
+
+    db.query((err, rows)=>{
+        if (err) {
+            res.status(500).send("OK");
+        } else {
+            res.status(200).send(rows);
+        }
+
+    })
+
+
+
+
+
 }
 export const verification = (req, res)=>{
     res.status(200).send("OK");
