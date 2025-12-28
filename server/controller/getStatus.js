52ca0d9278be8770ce2949d90f12606d34138c9f
lowhanfish <lowhanfish@gmail.com>
2025-12-28 09:57:37 +0800

    add dummy status

--- server/controller/getStatus.js
+++ server/controller/getStatus.js
@@ -1,1 +1,33 @@
 import db from "../db/mysql/index.js";
+
+
+
+
+export const canUpdate = ()=> {
+    return new Promise((resolve, reject) => {
+        const query = ``;
+        const values = [];
+        db.query(query, values, (err, rows)=> {
+            if (err){
+                reject(err)
+            }else{
+                resolve(rows)
+            }
+        })
+    })
+}
+
+
+export const dummyStatus = () => {
+    return new Promise((resolve, reject) => {
+        const query = ``;
+        const values = [];
+        db.query(query, values, (err, rows)=> {
+            if (err){
+                reject(err)
+            }else{
+                resolve(rows)
+            }
+        })
+    })
+}
